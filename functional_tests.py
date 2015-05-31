7b04dfafaf9b39b8fa2d8740fc9e93181199592c
pnicewicz421 <peter.nicewicz@gmail.com>
2015-05-31 09:29:00 -0400

    One line to-do lists

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Open webpage
 		self.browser.get('http://localhost:8000')
@@ -33,16 +38,15 @@ class NewVisitorTest(unittest.TestCase):
 		#Enter "buy peacock feathers" to the input field and send
 		inputbox.send_keys("Buy peacock feathers")
 		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys("Use peacock feathers to make a fly")
 		inputbox.send_keys(Keys.ENTER)
 		
 		#After a refresh, make sure that that input now appears in the row in the table
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
 		
 		self.fail('Finish the test!')
 		
