ec0960b9be5766afaa251e2b1f9f51c88e0dc761
pnicewicz421 <peter.nicewicz@gmail.com>
2015-05-31 09:21:08 -0400

    Incorporating POST method, and checking new_item_lists

--- functional_tests.py
+++ functional_tests.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest
+import test
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -32,12 +34,17 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys("Buy peacock feathers")
 		inputbox.send_keys(Keys.ENTER)
 		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys("Use peacock feathers to make a fly")
+		inputbox.send_keys(Keys.ENTER)
+		
 		#After a refresh, make sure that that input now appears in the row in the table
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = self.browser.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-		)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make fly', [row.text for row in rows])
+		
+		self.fail('Finish the test!')
 		
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -14,9 +14,33 @@ class homePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):		
 		request = HttpRequest()
 		response = home_page(request)
+		
 		expected_html = render_to_string('home.html')
+		#print (response.content.decode())
 		self.assertEqual(response.content.decode(), expected_html)
+		
 		#self.assertTrue(response.content.startswith(b'<html>'))
 		#self.assertIn(b'<title>To-Do lists</title>', response.content)
 		#self.assertTrue(response.content.strip().endswith(b'</html>')) 
 
+	def test_home_page_can_save_a_POST_request(self):
+		#setup
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		#exercise
+		response = home_page(request)
+		
+		#assert
+		self.assertIn('A new list item', response.content.decode()) 
+		expected_html = render_to_string(
+			'home.html', 
+			{'new_item_text': 'A new list item'}
+		)
+		#print (response.content.decode())
+		self.assertEqual(response.content.decode(), expected_html)
+
+		
+		
+		
