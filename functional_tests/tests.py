a10506382211463131ccf51a9b38b0a84b626fab
pnicewicz421 <peter.nicewicz@gmail.com>
2015-06-05 22:44:22 -0400

    make functional tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -5,13 +6,14 @@ import unittest
 import test
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 		
 	def tearDown(self):
+		pass
 		self.browser.quit()
 		
 	def check_for_row_in_list_table(self, row_text):
@@ -21,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Open webpage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 		
 		#"To-Do" in title and header
 		self.assertIn('To-Do', self.browser.title)
@@ -46,9 +48,6 @@ class NewVisitorTest(unittest.TestCase):
 		
 		#After a refresh, make sure that that input now appears in the row in the table
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make fly')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		
 		self.fail('Finish the test!')
-		
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
