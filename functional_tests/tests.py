56962489b0dd5a8114b7889ca8695b3841ecb143
pnicewicz421 <peter.nicewicz@gmail.com>
2015-06-28 19:52:25 -0400

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,9 +6,25 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 import test
 import time
+import sys
+
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return #
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+	
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -24,7 +40,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Open webpage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		
 		#"To-Do" in title and header
 		self.assertIn('To-Do', self.browser.title)
@@ -62,7 +78,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 		
 		#Francis visits the home page. No sign of Edith's list.
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -84,7 +100,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 	def test_layout_and_styling(self):
 		#Go to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 		
 		#make sure inputbox is centered
