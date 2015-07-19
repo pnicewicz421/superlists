cf27ce3a8624020ef6e2b69faabb65ff378bec0e
Przemy <peter.nicewicz@gmail.com>
2015-07-19 18:20:55 -0400

    Item List Validation Test Filled in But Has-Error method not fixed

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,16 +5,28 @@ class ItemValidationTest(FunctionalTest):
 	def test_cannot_add_empty_list_items(self):
 		#Edith goes to the home page and accidentally tries to submit
 		#an empty list item. She hits Enter
+		self.browser.get(self.server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 		
 		#The home page refreshes. There is an error message
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 		
 		#She tries again with some text, which works
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+		self.check_for_row_in_list_table('1: Buy milk')
 		
 		#Blank again, which again 
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 		
 		#gives a warning message
+		self.check_for_row_in_list_table('1: Buy milk')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 		
 		#She corrects it by filling in some text
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+		self.check_for_row_in_list_table('1: Buy milk')
+		self.check_for_row_in_list_table('2: Make tea')
 		
-		self.fail('write me!')
 		
